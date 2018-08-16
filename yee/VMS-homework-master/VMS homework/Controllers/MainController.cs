3c3345e5da897e07a4819b7e9b763787556b5101
Camille <camille.soliman@gmail.com>
2018-08-16 16:07:16 -0400

    fak

---
+++ yee/VMS-homework-master/VMS homework/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMS_homework.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        [Authorize]
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+
+        public ActionResult ManageOpp()
+        {
+            return View();
+        }
+    }
+}
