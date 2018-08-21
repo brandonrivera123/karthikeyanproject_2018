1f6d73f1c58de0eaa7f283c80620a4b203609ab6
Rivera <n00939999@ospreys.unf.edu>
2018-08-20 20:47:47 -0400

    Summer Volunteer Application

---
+++ VolunteerOpportunities/VolunteerOpportunities/Controllers/OpportunitiesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VolunteerOpportunities.Models;
+
+namespace VolunteerOpportunities.Controllers
+{
+    public class OpportunitiesController : Controller
+    {
+        private ODBContext db = new ODBContext();
+
+        // GET: Opportunities
+        public ActionResult Index()
+        {
+            return View(db.Opportunities.ToList());
+        }
+
+        // GET: Opportunities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Opportunity opportunity = db.Opportunities.Find(id);
+            if (opportunity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(opportunity);
+        }
+
+        // GET: Opportunities/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Opportunities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OID,OName,OStreetAddress,OState,OZipCode,OSkill,OTimes,OCollege")] Opportunity opportunity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Opportunities.Add(opportunity);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(opportunity);
+        }
+
+        // GET: Opportunities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Opportunity opportunity = db.Opportunities.Find(id);
+            if (opportunity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(opportunity);
+        }
+
+        // POST: Opportunities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OID,OName,OStreetAddress,OState,OZipCode,OSkill,OTimes,OCollege")] Opportunity opportunity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(opportunity).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(opportunity);
+        }
+
+        // GET: Opportunities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Opportunity opportunity = db.Opportunities.Find(id);
+            if (opportunity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(opportunity);
+        }
+
+        // POST: Opportunities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Opportunity opportunity = db.Opportunities.Find(id);
+            db.Opportunities.Remove(opportunity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VolunteerOpportunities/VolunteerOpportunities/Models/Volunteer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace VolunteerOpportunities.Models
+{
+    public class Volunteer
+    {
+        public int ID { get; set; }
+
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string VolunteerLocation { get; set; }
+        public string Skills { get; set; }
+        public string Availability { get; set; }
+
+        public string Address { get; set; }
+        public string HomeNumber { get; set; }
+        public string CellNumber { get; set; }
+        public string WorkNumber { get; set; }
+        public string Email { get; set; }
+        public string Education { get; set; }
+        public string Licenses { get; set; }
+
+        public string EmergnecyName { get; set; }
+        public string EmergencyNumber { get; set; }
+        public string EmergencyEmail { get; set; }
+        public string EmergencyAddress { get; set; }
+
+        public bool DLFile { get; set; }
+        public bool SSNFile { get; set; }
+        public string ApprovalStatus { get; set; }
+    }
+
+    public class VolunteerDBContext : DbContext
+    {
+        public DbSet<Volunteer> Volunteers { get; set; }
+    }
+
+}
